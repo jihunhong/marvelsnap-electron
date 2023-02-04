a033a2481c2295e14e85a947513ce844e09e2fc9
jihunhong <redgee49@gmail.com>
2023-02-05 01:33:37 +0900

    이용자 컬렉션받아서 백엔드 호출하는거까지

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const { app, shell, BrowserWindow, Tray, Menu, nativeImage, Notification, dialog } = require("electron");
-const path = require('path');
-const fs = require('fs');
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  nativeImage,
+  Notification,
+} = require("electron");
+const path = require("path");
+const fs = require("fs");
 const { parseAsJSONIfNeeded } = require("./events");
+const { default: axios } = require("axios");
 
-app.whenReady().then(() => {
-  const win = new BrowserWindow({ show: false });
-  console.log(win.isVisible());
-  win.hide();
-  settingTray();
-}).then(() => {
-  if (process.platform === 'win32') {
-    if(process.argv[1] == '--squirrel-firstrun'){
+app
+  .whenReady()
+  .then(() => {
+    settingTray();
+  })
+  .then(() => {
+    if (process.platform === "win32") {
+      // if (process.argv[1] == "--squirrel-firstrun")
       showNotification();
     }
-  }
-})
-
+  });
 
 function settingTray() {
   const iconPath = path.join(__dirname, "./electron.png");
@@ -29,13 +35,13 @@ function settingTray() {
       },
     },
     {
-      label: '시작프로그램 등록',
-      type: 'normal',
+      label: "시작프로그램 등록",
+      type: "normal",
       click() {
         app.setLoginItemSettings({
-          openAtLogin: true
-        })
-      }
+          openAtLogin: true,
+        });
+      },
     },
     {
       label: "종료",
@@ -46,14 +52,36 @@ function settingTray() {
   tray.setContextMenu(contextMenu);
 }
 
-function showNotification({ title = 'Welcome', body = 'Snapsco에 컬렉션 데이터를 연동 할 수 있습니다'}) {
+function showNotification({
+  title = "Welcome",
+  body = "Snapsco에 컬렉션 데이터를 연동 할 수 있습니다",
+}) {
   new Notification({ title, body }).show();
 }
 
-function dataSync(){
-  const filePath = path.join(app.getPath('home'), 'AppData', 'LocalLow', 'Second Dinner', 'SNAP', 'Standalone', 'States', 'nvprod', 'CollectionState.json');
-  const file = fs.readFileSync(filePath, { encoding: 'utf8'} );
+function dataSync() {
+  const filePath = path.join(
+    app.getPath("home"),
+    "AppData",
+    "LocalLow",
+    "Second Dinner",
+    "SNAP",
+    "Standalone",
+    "States",
+    "nvprod",
+    "CollectionState.json"
+  );
+  const file = fs.readFileSync(filePath, { encoding: "utf8" });
   const json = parseAsJSONIfNeeded(file);
   const profileId = json.ServerState.Account.Id;
-  shell.openExternal(`https://snapsco.net/p/${profileId}`)
+  const cards = Array.from(new Set(json.Cards.map((c) => c.CardDefId)));
+  axios
+    .post(`${process.env.END_POINT}/api/collections/user_collection/records`, {
+      profileId,
+      cards: JSON.stringify(cards),
+    })
+    .then((res) => {
+      shell.openExternal(`https://snapsco.net/p/${profileId}`);
+    })
+    .catch((err) => {});
 }
