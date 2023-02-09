5905861f5ff6b92f42497b6f4cef6984105faf55
jihunhong <redgee49@gmail.com>
2023-02-09 17:56:13 +0900

    update request 수정

--- api.js
+++ api.js
@@ -1,65 +1,69 @@
-const {default: axios} = require("axios")
-const {shell} = require("electron")
-
+const { default: axios } = require("axios");
+const { shell } = require("electron");
 
 /**
- * 
- * @param {string} profileId 
+ *
+ * @param {string} profileId
  */
-exports.getUser = function(profileId) {
-    return new Promise((resolve, reject) => {
-        axios.get(`https://api.snapsco.net/api/collections/user_collection/record?filter=(profileId="${profileId}")&perPage=1`)
-            .then(response => {
-                resolve(response.data.items[0].id)
-            })
-            .catch((err) => {
-                reject(err);
-            })
-    })
-}
+exports.getUser = function (profileId) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        `https://api.snapsco.net/api/collections/user_collection/record?filter=(profileId="${profileId}")&perPage=1`
+      )
+      .then((response) => {
+        resolve(response.data.items[0].id);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
 /**
- * 
- * @param {string} id 
- * @param {string} profileId 
- * @param {string[]} cards 
- */
-exports.update = function(id, profileId, cards) {
-    return new Promise((resolve, reject) => {
-        axios
-            .post(`https://api.snapsco.net/api/collections/user_collection/records/${id}`, {
-                profileId,
-                cards: JSON.stringify(cards),
-            })
-            .then((response) => {
-                shell.openExternal(`https://snapsco.net/p/${profileId}`);
-                resolve(response);
-            })
-            .catch((err) => {
-                reject(err)
-            });
-    })
-}
-
-/**
- * 
+ * update request
  * @param {string} id
- * @param {string} profileId 
- * @param {string[]} cards 
+ * @param {string} profileId
+ * @param {string[]} cards
  */
-exports.create = function(profileId, cards) {
-    return new Promise((resolve, reject) => {
-        axios
-            .post(`https://api.snapsco.net/api/collections/user_collection/records`, {
-                profileId,
-                cards: JSON.stringify(cards),
-            })
-            .then((response) => {
-                shell.openExternal(`https://snapsco.net/p/${profileId}`);
-                resolve(response);
-            })
-            .catch((err) => {
-                reject(err)
-            });
-    })
-}
+exports.update = function (id, profileId, cards) {
+  return new Promise((resolve, reject) => {
+    axios
+      .patch(
+        `https://api.snapsco.net/api/collections/user_collection/records/${id}`,
+        {
+          profileId,
+          cards: JSON.stringify(cards),
+        }
+      )
+      .then((response) => {
+        shell.openExternal(`https://snapsco.net/p/${profileId}`);
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+/**
+ * create request
+ * @param {string} profileId
+ * @param {string[]} cards
+ */
+exports.create = function (profileId, cards) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`https://api.snapsco.net/api/collections/user_collection/records`, {
+        profileId,
+        cards: JSON.stringify(cards),
+      })
+      .then((response) => {
+        shell.openExternal(`https://snapsco.net/p/${profileId}`);
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
